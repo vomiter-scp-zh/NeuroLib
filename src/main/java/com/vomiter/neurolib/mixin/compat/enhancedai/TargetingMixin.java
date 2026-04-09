6397ca68c4cdd9cf90079823755ecef5a98a9b74
vomiter-scp-zh <97169118+vomiter-scp-zh@users.noreply.github.com>
2026-04-09 09:53:37 +0800

    eai compat

--- src/main/java/com/vomiter/neurolib/mixin/compat/enhancedai/TargetingMixin.java
+++ src/main/java/com/vomiter/neurolib/mixin/compat/enhancedai/TargetingMixin.java
@@ -3,7 +3,6 @@ package com.vomiter.neurolib.mixin.compat.enhancedai;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.vomiter.neurolib.common.entity.hunt.AbstractCappedHuntGoal;
-import com.vomiter.neurolib.common.entity.stalker.StalkerNearestAttackableTargetGoal;
 import insane96mcp.enhancedai.modules.mobs.targeting.Targeting;
 import net.minecraft.world.entity.ai.goal.Goal;
 import net.minecraft.world.entity.ai.goal.WrappedGoal;
@@ -15,7 +14,6 @@ public class TargetingMixin {
     @WrapOperation(method = "processTargetGoal", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/ai/goal/WrappedGoal;getGoal()Lnet/minecraft/world/entity/ai/goal/Goal;", ordinal = 0))
     private Goal neurolib$interceptGoal(WrappedGoal instance, Operation<Goal> original){
         var originalResult = original.call(instance);
-        if(originalResult instanceof StalkerNearestAttackableTargetGoal) return null;
         if(originalResult instanceof AbstractCappedHuntGoal<?,?>) return null;
         return originalResult;
     }
